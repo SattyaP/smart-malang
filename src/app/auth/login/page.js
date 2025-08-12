194c7f40045889bb1a25e67332e1d006b603a867
SattyaP <stylearning7@gmail.com>
2025-08-12 21:11:43 +0700

    Implement layout structure with Navbar and Footer; add Login and Register pages with forms

---
+++ src/app/auth/login/page.js
@@ -0,0 +1,50 @@
+import Link from 'next/link'
+
+export const metadata = {
+  title: 'Login | Smart Malang',
+}
+
+export default function LoginPage() {
+  return (
+    <div className="hero min-h-[70vh]">
+      <div className="hero-content flex-col lg:flex-row">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card bg-base-100 w-full max-w-xl shrink-0 shadow-2xl">
+          <div className="card-body">
+            <fieldset className="fieldset">
+              <label className="label text-md">Email</label>
+              <input
+                type="email"
+                className="input w-full"
+                placeholder="Email"
+              />
+              <label className="label text-md">Password</label>
+              <input
+                type="password"
+                className="input w-full"
+                placeholder="Password"
+              />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn btn-neutral mt-4">Login</button>
+              <div className="mt-2">
+                <span>Dont have an account? </span>
+                <Link className="link link-hover" href="/auth/register">
+                  Register
+                </Link>
+              </div>
+            </fieldset>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
