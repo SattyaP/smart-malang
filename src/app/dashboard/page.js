82a18a4f7ec138057dba42fecdadfcae652c5d0f
hanifkaromi14 <hanifkaromi@gmail.com>
2025-08-17 19:03:23 +0700

    integrate maps with mapbox token2

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,20 @@
+"use client";
+
+import Map, { Marker } from "react-map-gl";
+
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
-  )
+    <Map
+      initialViewState={{
+        longitude: 112.6326,
+        latitude: -7.9666,
+        zoom: 12,
+      }}
+      style={{ width: "100%", height: 500 }}
+      mapStyle="mapbox://styles/mapbox/streets-v11"
+      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+    >
+      {/* <Marker longitude={112.6326} latitude={-7.9666} color="red" /> */}
+    </Map>
+  );
 }
